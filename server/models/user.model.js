c626a2f2922d1626fe86a62297ac01dc0a1b45e5
FreshJourney <freshjourney00@gmail.com>
2025-03-20 22:48:04 +0530

    User Model Added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema(
+  {
+    UserName: {
+      type: String,
+      required: true,
+    },
+    Email: {
+      type: String,
+      required: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = UserSchema;
